b77f739789fb1026210d515df4cb6dbee2c6d05b
lưn <luanho4568@gmail.com>
2024-08-27 21:17:13 +0700

    first commit

---
+++ 01-next-rn/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+
+export default function Home() {
+    return (
+        <div>
+            hello world nextjs with hoidanit
+            <div></div>
+            <div>
+                <Button type="primary"> Add new</Button>
+            </div>
+        </div>
+    );
+}
