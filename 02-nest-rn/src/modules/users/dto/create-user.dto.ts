f8d2f6ca61178578b11d0eeadffaac6ad050e778
lưn <luanho4568@gmail.com>
2024-08-28 23:04:51 +0700

    CRUD  User 3.10

--- 02-nest-rn/src/modules/users/dto/create-user.dto.ts
+++ 02-nest-rn/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({message : "Name cannot be blank"})
+  name: string;
+  
+  @IsNotEmpty()
+  @IsEmail({} , {message:"Email is not in correct format"})
+  email: string;
+
+  @IsNotEmpty({message : "Password cannot be blank"})
+  password: string;
+
+  phone: string;
+  address: string;
+  image: string;
+}
--- 02-nest-rn/src/modules/users/users.service.ts
+++ 02-nest-rn/src/modules/users/users.service.ts
@@ -1,15 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { hashPasswordHelper } from '@/helpers/util';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  // check email exist
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.exists({ email });
+    if (!user) return false;
+    return true;
+  };
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+    // check email
+    const isExist = await this.isEmailExist(email);
+    if (isExist)
+      throw new BadRequestException(
+        `Email '${email}' already exists . Please use another email!`,
+      );
+    //hash password
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      image,
+    });
+    return {
+      _id: user._id,
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: string) {
+    const { filter, limit, skip, sort } = aqp(query);
+    const result = await this.userModel
+      .find(filter)
+      .limit(limit)
+      .skip(skip)
+      .sort(sort as any);
+    return result;
   }
 
   findOne(id: number) {
