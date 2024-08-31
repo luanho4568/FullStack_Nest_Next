b42e4e90fbcc67eb42f9195c41e3ff91041ad138
lưn <luanho4568@gmail.com>
2024-08-31 11:20:55 +0700

    Setup Next-auth (authjs)

--- 01-next-rn/src/components/auth/login.tsx
+++ 01-next-rn/src/components/auth/login.tsx
@@ -1,37 +1,44 @@
-'use client'
-import { Button, Col, Divider, Form, Input, Row } from 'antd';
-import { ArrowLeftOutlined } from '@ant-design/icons';
-import Link from 'next/link';
+"use client";
+import { Button, Col, Divider, Form, Input, Row } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { authenticate } from "@/utils/actions";
 
 const Login = () => {
-
     const onFinish = async (values: any) => {
+        console.log(">>> check values: ", values);
+        const { email, password } = values;
+        // trigger sign-in
+        const res = await authenticate(email, password);
+        console.log(res);
+        
 
+
+        // const data = await signIn("credentials", { email, password ,redirect : false});
+        // console.log(">>> check data: " , data);
+        
     };
 
     return (
         <Row justify={"center"} style={{ marginTop: "30px" }}>
             <Col xs={24} md={16} lg={8}>
-                <fieldset style={{
-                    padding: "15px",
-                    margin: "5px",
-                    border: "1px solid #ccc",
-                    borderRadius: "5px"
-                }}>
+                <fieldset
+                    style={{
+                        padding: "15px",
+                        margin: "5px",
+                        border: "1px solid #ccc",
+                        borderRadius: "5px",
+                    }}
+                >
                     <legend>Đăng Nhập</legend>
-                    <Form
-                        name="basic"
-                        onFinish={onFinish}
-                        autoComplete="off"
-                        layout='vertical'
-                    >
+                    <Form name="basic" onFinish={onFinish} autoComplete="off" layout="vertical">
                         <Form.Item
                             label="Email"
                             name="email"
                             rules={[
                                 {
                                     required: true,
-                                    message: 'Please input your email!',
+                                    message: "Please input your email!",
                                 },
                             ]}
                         >
@@ -44,23 +51,22 @@ const Login = () => {
                             rules={[
                                 {
                                     required: true,
-                                    message: 'Please input your password!',
+                                    message: "Please input your password!",
                                 },
                             ]}
                         >
                             <Input.Password />
                         </Form.Item>
 
-
-
-                        <Form.Item
-                        >
+                        <Form.Item>
                             <Button type="primary" htmlType="submit">
                                 Login
                             </Button>
                         </Form.Item>
                     </Form>
-                    <Link href={"/"}><ArrowLeftOutlined /> Quay lại trang chủ</Link>
+                    <Link href={"/"}>
+                        <ArrowLeftOutlined /> Quay lại trang chủ
+                    </Link>
                     <Divider />
                     <div style={{ textAlign: "center" }}>
                         Chưa có tài khoản? <Link href={"/auth/register"}>Đăng ký tại đây</Link>
@@ -68,7 +74,7 @@ const Login = () => {
                 </fieldset>
             </Col>
         </Row>
-    )
-}
+    );
+};
 
 export default Login;
